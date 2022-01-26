67b3a159b20e350101b1b1760153839c99d1cbd7
gsion1 <gsion@thingva.com>
2022-01-26 19:09:45 +0100

    remove functions in init_graph

--- js/init_graph.js
+++ js/init_graph.js
@@ -36,50 +36,5 @@ function cy_create(c) {
 
 }
 
-function extract_graph(){
-    var edges = cy.edges()
-    var res = []
-    for(let i=0; i<edges.length;i++) {
-        var json = cy.data(edges[i].json())
-        var source = json.data()['data']['source']
-        var target = json.data()['data']['target']
-        res.push([source, target])
-    }
-    return res
-}
 
-//must be shorten. No internet, have to recycle
-/*
-function get_type_from_edge(edge){
-    var edges = cy.edges()
 
-    for(let i=0; i<edges.length;i++) {
-        var json = cy.data(edges[i].json())
-        if ( (edge[0] == json.data()['data']['source'] && edge[1] == json.data()['data']['target']) || (edge[0] == json.data()['data']['target'] && edge[1] == json.data()['data']['source'])) {
-            var type = json.data()['data']['type']
-            return type
-        }
-    }
-    return 'error'
-}*/
-
-function get_edges_from_nodes(node1, node2){
-    var edges = cy.edges()
-    var res = []
-    for(let i=0; i<edges.length;i++) {
-        var json = cy.data(edges[i].json())
-        var source = json.data()['data']['source']
-        var target = json.data()['data']['target']
-        var type = json.data()['data']['type']
-        var point = json.data()['data']['point']
-        if((source == node1 && target == node2) || (source == node2 && target == node1) ) { res.push(json.data()['data']) }
-    }
-    return res
-}
-
-function get_cf(){
-    var val = 0.1
-    var axis = "v"
-    var point = [10,20,30]
-    return {val: val, axis: axis, point:point}
-}
