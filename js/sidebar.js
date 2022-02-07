6816cc97d15a5818ace7d5e8220207840143af71
gsion1 <gsion@thingva.com>
2022-02-07 20:04:22 +0100

    sidebar now works better

---
+++ js/sidebar.js
@@ -0,0 +1,44 @@
+let sidebar_items = [["img/graph.png", "Graphs","joint-graph.html"],["img/tol.png", "Graphs","cytoscape.html"], ["img/cut.png", "CF", "cuts.html"],
+["img/link.png", "Liaisons composées", "joint-composition.html"]];
+
+function appendToSidebar(container) {
+    let html = document.getElementById(container).innerHTML;
+    for(let i=0; i<sidebar_items.length; i++){
+        html += `<div class="sidebar-item"><a href="${sidebar_items[i][2]}"><img class="sidebar-logo" src="${sidebar_items[i][0]}"/><div class="sidebar-text">${sidebar_items[i][1]}</div></a></div>`
+    }
+    document.getElementById(container).innerHTML = html;
+
+}
+
+function toggleSidebar(){
+    let state = document.getElementById("sidebar-btn").innerHTML;
+    console.log(state)
+    if (state.search("☰") != -1) {//closed
+        openNav();
+        console.log("opening navbar")
+    }
+    else {
+        closeNav();
+        console.log("closing navbar")
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidebar-btn").innerHTML = "×";
+    let texts = document.getElementsByClassName("sidebar-text")
+    for(t of texts){
+        t.style.display = "block";
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "50px";
+    document.getElementById("main").style.marginLeft= "50px";
+    document.getElementById("sidebar-btn").innerHTML = "&#9776;";
+    let texts = document.getElementsByClassName("sidebar-text")
+    for(t of texts){
+        t.style.display = "none";
+    }
+}
