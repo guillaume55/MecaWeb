da4fc9a2166a7b0f32f060d7c4d3fa3c55da38a1
gsion1 <gsion@thingva.com>
2022-02-09 18:30:05 +0100

    translation

--- translation/translation.js
+++ translation/translation.js
@@ -1,15 +1,32 @@
-function getLang(filename){
-    var allText =[];
-    var allTextLines = [];
-    var Lines = [];
-    var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", filename, true);
-    txtFile.onreadystatechange = function()
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+let lang = {}
+
+function getLang(url){
+    
+    reader.open('get', url, true); 
+    reader.onreadystatechange = displayContents;
+    reader.send(null);
+
+    /*txtFile.onreadystatechange = function()
     {
+        console.log("hey", allTextLines);
         allText = txtFile.responseText;
         allTextLines = allText.split(/\r\n|\n/);
-        console.log("hey", allTextLines);
-    };
+        
+    };*/
     
     
+}
+
+function displayContents() {
+    if(reader.readyState==4) {
+        let text = reader.responseText;
+        console.log(text)
+        let lines = text.split(/\r\n|\n/);
+        for(l of lines){
+            var item = l.split(",")
+            lang[item[0]] = item[1]
+        }
+        console.log(lang)
+    }
 }
