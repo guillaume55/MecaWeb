7470f4506516346da891d60800ffaa77d9c950bf
gsion1 <gsion@thingva.com>
2022-02-09 19:36:58 +0100

    translation transmission ok !

--- translation/translation.js
+++ translation/translation.js
@@ -1,39 +1,51 @@
 var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
-let L = {}
 
-function getLang(url){
-    
-    reader.open('get', url, true); 
-    reader.onreadystatechange = displayContents;
-    reader.send(null);
-
-    /*txtFile.onreadystatechange = function()
-    {
-        console.log("hey", allTextLines);
-        allText = txtFile.responseText;
-        allTextLines = allText.split(/\r\n|\n/);
-        
-    };*/
-    
-    
+function translate(){
+    var lang = document.cookie['lang']
+    if(lang == undefined) {
+        lang = navigator.language || navigator.userLanguage;
+    }
+    //get name of the windows
+    var currenturl = window.location.pathname.split("/")
+    var htmlfile = currenturl[currenturl.length-1].split(".")[0]
+    try {
+        url = `https://guillaume55.github.io/MecaWeb/${lang}/${htmlfile}.csv`
+        reader.open('get', url, true); 
+        reader.onreadystatechange = parseCsv;
+        reader.send(null);
+    }catch{
+        console.log("Cannot translate") 
+        //remove at least the braces
+        //keep replacing because it will be a disaster with csv if we modify/correct what is inside braces
+        url = `https://guillaume55.github.io/MecaWeb/en/${htmlfile}.csv`
+        reader.open('get', url, true); 
+        reader.onreadystatechange = parseCsv;
+        reader.send(null);
+    }
 }
 
-function displayContents() {
+function parseCsv() {
     if(reader.readyState==4) {
         let text = reader.responseText;
+        let L = {}
         console.log(text)
         let lines = text.split(/\r\n|\n/);
         for(l of lines){
             var item = l.split(",")
             L[item[0]] = item[1]
         }
+        translateReplace(L)
     }
 }
 
-function translate(language){
-    html = document.getElementsByTagName("BODY")[0].innerHTML;
+function translateReplace(){
+    
+    html = document.body.innerHTML;
     for(k of Object.keys(L)) {
-        html.replaceAll(k,L[k])
+        html = html.replaceAll("{"+k+"}",L[k])
+        console.log(document.body.innerHTML.search(k))
+        //console.log(k,L[k])
     }
-
+    document.body.innerHTML = html;
+    //console.log(html)
 }
