8198367d8413f585f85dbba79aaf07592a387aa9
gsion1 <gsion@thingva.com>
2022-02-09 17:49:00 +0100

    added linspace function

--- js/simple_math_tools.js
+++ js/simple_math_tools.js
@@ -3,3 +3,11 @@ function roundDec(nombre, precision){
 	var tmp = Math.pow(10, precision);
 	return Math.round( nombre*tmp )/tmp;
 }
+
+//num is number of elements in array, from stackoverflowfrom user STJ
+//https://stackoverflow.com/questions/40475155/does-javascript-have-a-method-that-returns-an-array-of-numbers-based-on-start-s 
+function linspace(start, stop, num, endpoint = true) {
+	const div = endpoint ? (num - 1) : num;
+	const step = (stop - start) / div;
+	return Array.from({length: num}, (_, i) => start + step * i);
+}
