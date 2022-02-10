050331a8068fcd3accb5a48bc60a39858725c9a2
gsion1 <gsion@thingva.com>
2022-02-10 21:10:21 +0100

    buckling

---
+++ js/struct/arcB.js
@@ -0,0 +1,54 @@
+function computeArcB(){
+    let f_coef = parseFloat(document.getElementById('arcB_coef').value);
+    let longueur = parseFloat(document.getElementById('arcB_length').value);
+    let bl = parseFloat(document.getElementById('arcB_bl').value);
+
+    let d_computed = longueur / (2*f_coef)
+    document.getElementById('resArcB').innerHTML = d_computed < bl ? "Blocked" : "Sliding"
+
+}
+
+function selectArcB(val){
+    let coef_friction = getFriction()
+    if(val == "custom"){
+        //document.getElementById('arcB_coef').style.display ="none";
+    }else {
+        //document.getElementById('arcB_coef').style.display ="block";
+        document.getElementById('arcB_coef').value = coef_friction[val];
+    }
+}
+
+function getFriction(){
+    let coef_friction = {"custom":"","steel-steel-dry":0.15,"steel-steel-lub":0.12,
+                         "steel-al-dry":0.35,"steel-Mo-S2":0.16,
+                         "steel-bronze-dry":0.19,"steel-steel-lub":0.11,
+                         "steel-bronze-dry":0.19,"steel-bronze-lub":0.11,
+                         "steel-graphite-dry":0.20,
+                         "steel-polyethylene-dry":0.30,"steel-PTFE-dry":0.4,
+                         "steel-polyurethane-dry":0.50,"steel-polyurethane-lub":0.20}
+    return coef_friction
+}
+
+function fillSelect(id, data){
+    s = document.getElementById(id)
+    //let options = getFriction()
+    for(k of Object.keys(data)) {
+        var c = document.createElement("option");
+        c.text = k;
+        s.options.add(c, 1);
+    }
+}
+
+function fillSelectGroup(id, data){
+    s = document.getElementById(id)
+    for(k of Object.keys(data)){
+        let g = document.createElement("OPTGROUP");
+        g.setAttribute("label", k);
+        for(k2 of Object.keys(data[k])) {
+            var c = document.createElement("option");
+            c.text = k2;
+            g.appendChild(c)
+        }
+        s.appendChild(g)
+    }
+}
---
+++ js/struct/buckling.js
@@ -0,0 +1,101 @@
+function computeBuckling(){
+    let len = parseFloat(document.getElementById('buckling_length').value);
+    let young = parseFloat(document.getElementById('buckling_young').value);
+    let load = parseFloat(document.getElementById('buckling_force').value);
+    let thickness = parseFloat(document.getElementById("buckling_beamThick").value);
+    let width = parseFloat(document.getElementById("buckling_beamWidth").value);
+    let height = parseFloat(document.getElementById("buckling_beamHeight").value);
+    let diam = parseFloat(document.getElementById("buckling_beamDiam").value);
+
+    let mounting = getRadio('buckling_mounting')
+
+    let inertia = computeInertia()
+
+    //units !!! in newton
+    let eulerCriticalLoad = (Math.PI^2 * young*1000 * inertia)/len^2
+
+    document.getElementById('buckling_eulerCritical').innerHTML = eulerCriticalLoad
+}
+
+function selectYoung(val){
+    let materials = getMaterials()
+    let young = 0
+    for(k of Object.keys(materials)){
+        if(materials[k][val] != undefined){
+            young = materials[k][val];
+        }
+    }
+
+    if(val == "custom"){
+        //document.getElementById('arcB_coef').style.display ="none";
+    }else {
+        //document.getElementById('arcB_coef').style.display ="block";
+        document.getElementById('buckling_young').value = young;
+    }
+}
+
+function getMaterials(){
+    let steels = {"Stainless":195,"A36":200}
+    let al = {"Al 1100":69,"Al 2024":72.4,"Al 6061":69,"Al7075":71}
+    let otherMetals = {"Brass":106,"Bronze":112,"Inconel":207,"Copper":110,"Gold":77.2,"Mg alloy":45.2,"Nickel":200, "Ti Alloy Ti- 6AL-4V":114}
+    let plastics = {"HDPE":1.1,"LDPE":0.228,"PC":2.2,"PP":1.68,"PET":3.14,"Nylon 66":2.93}
+    let other = {"Human bone":14,"Diamond":1130}
+    let materials = {"":{"custom":""},"Steel":steels,"Aluminum":al,"Other metals":otherMetals,"Plastics":plastics, "Other": other}
+    
+    return materials
+}
+
+function showBeamParams(beamType) {
+    if(beamType.search("rect")!=-1) {
+        document.getElementById("buckling_beamHeight_div").style.display="Block";
+        document.getElementById("buckling_beamWidth_div").style.display="Block";
+        document.getElementById("buckling_beamDiam_div").style.display="none";
+    }
+    if(beamType.search("circ")!=-1) {
+        document.getElementById("buckling_beamHeight_div").style.display="none";
+        document.getElementById("buckling_beamWidth_div").style.display="none";
+        document.getElementById("buckling_beamDiam_div").style.display="block";
+    }
+    if(beamType.search("pipe")!=-1) {
+        document.getElementById("buckling_beamThick_div").style.display="Block";
+    }else {
+        document.getElementById("buckling_beamThick_div").style.display="none";
+    }
+}
+
+function getRadio(name){
+    radios = document.getElementsByName(name)
+    for(e of radios){
+        if(e.checked==true)
+            return e.value
+    }
+}
+
+function computeInertia(){
+    let thickness = parseFloat(document.getElementById("buckling_beamThick").value);
+    let width = parseFloat(document.getElementById("buckling_beamWidth").value);
+    let height = parseFloat(document.getElementById("buckling_beamHeight").value);
+    let diam = parseFloat(document.getElementById("buckling_beamDiam").value);
+
+    let beamType = getRadio("bucklingSection")
+    console.log(beamType)
+    let inertia = 0
+
+    if(beamType == "circ") {
+        inertia = (Math.PI*diam^4)/64
+    }
+    if(beamType == "circ_pipe") {
+        inertia = (Math.PI*diam^4)/64 - (Math.PI*(diam-thickness*2)^4)/64
+    }
+    if(beamType == "rect") {
+        inertia = (width*height^3)/12
+    }
+    if(beamType == "rect_pipe") {
+        inertia = (width*height^3)/12-inertia - ((width-thickness*2)*(height-thickness*2)^3)/12
+    }
+
+    document.getElementById('buckling_resInertia').innerHTML = roundDec(inertia)
+    document.getElementById('buckling_resInertia2').innerHTML = roundDec(inertia)
+    return inertia
+}
+
