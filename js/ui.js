243204c24c06ade096a189d0ac57fa0289091bd3
gsion1 <gsion@thingva.com>
2022-03-09 21:32:53 +0100

    add opentabAtsartup

--- js/ui.js
+++ js/ui.js
@@ -6,4 +6,16 @@ function showElt(id){
     document.getElementById(id).style.display = "block"
 }
 
-
+function openTabStartUp(){
+    var url = window.location.href;
+    var tab = 0
+    try{
+        console.log(url)
+        let searchParams = new URLSearchParams(url);
+        var tab = searchParams.get("tab");
+    }
+    finally {
+        document.getElementsByClassName('tablinks')[parseInt(tab)].click() //open tab
+    }
+    console.log("ab",tab)
+}
