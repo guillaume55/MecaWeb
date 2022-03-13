4fa5c8c426fb69d5a24382effee5b018d9310201
gsion1 <gsion@thingva.com>
2022-03-13 17:02:14 +0100

    add bearing mounting

--- js/transmission/bearing-mounting.js
+++ js/transmission/bearing-mounting.js
@@ -1,8 +1,42 @@
 function bearingChoice_findBest(){
     let dir = parseFloat(document.getElementById('bearingChoice_axialLoadSide').value);
-    let Fa = parseFloat(document.getElementById('bearingChoice_axialLoad').value);
-    let Fr = parseFloat(document.getElementById('bearingChoice_radialLoad').value);
-    let perf = parseFloat(document.getElementById('bearingChoice_perf').value);
+    //let Fa = parseFloat(document.getElementById('bearingChoice_axialLoad').value);
+    //let Fr = parseFloat(document.getElementById('bearingChoice_radialLoad').value);
+    let radAx = document.getElementById('bearingChoice_RadAx').value;
+    let perf = document.getElementById('bearingChoice_perf').value;
+
+    let res = {"bearingChoice_12":5, "bearingChoice_34":5,"bearingChoice_5":5,"bearingChoice_67":5}
+    if(dir==2){
+        res['bearingChoice_34']-=3
+        res['bearingChoice_67']-=4
+    }
+    if(perf==1) //economical
+    {
+        res['bearingChoice_67']+=3
+        res['bearingChoice_5']-=3
+    }
+    else if(perf==3) //high perf
+    {
+        res['bearingChoice_67']-=3
+        res['bearingChoice_5']+=3
+    }
+    if(radAx >3){
+        res['bearingChoice_67']-=3
+        res['bearingChoice_34']-=2
+        res['bearingChoice_5']+=radAx
+    }
+    
+    //Object.keys(res).sort()
+    for(m of Object.keys(res)){
+        if(res[m] > 10){ res[m]="&#x1f3c6;" /*trophy */}
+        else if (res[m] > 4) { res[m]="&#9745;" /*checkbox checked */}
+        else{ res[m]="&#9587;" /*Cross  */}
+        console.log(m, res[m])
+        document.getElementById(m).innerHTML = res[m]
+    }
+
+
+
 }
 
 function bearingChoice_refreshSliderVal(val, slider){
@@ -14,6 +48,6 @@ function bearingChoice_refreshSliderVal(val, slider){
     }
     else if(slider=="radAx"){
         let data = [T['Mainly radial'],T['More radial'],T['Equilibrated'],T['More axial'],T['Mainly axial']]
-        document.getElementById('bearingChoice_RadAx').innerHTML = data[val-1]
+        document.getElementById('bearingChoice_RadAxVal').innerHTML = data[val-1]
     }
 }
