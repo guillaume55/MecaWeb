7db0647f45044986d2fd90e3146d05286db79e00
gsion1 <gsion@thingva.com>
2022-03-23 19:50:59 +0100

    add calculaor

---
+++ js/calculator.js
@@ -0,0 +1,13 @@
+function toggleCalculator(){
+    //let position = parseFloat(document.getElementById('calculator_table').style.right);
+    //var ifrm = document.getElementById('embed_calculator')
+    //var doc = ifrm.contentDocument? ifrm.contentDocument:
+        //ifrm.contentWindow.document
+    let position = parseFloat(document.getElementById('embed_calculator').style.right);
+    console.log(position)
+    if(position >= 0){
+     document.getElementById('embed_calculator').style.right = "-305px";
+    } else {
+     document.getElementById('embed_calculator').style.right = "5px";
+    }
+}
