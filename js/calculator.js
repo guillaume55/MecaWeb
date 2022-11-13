84852ea6253030544d8283092a107146b291c7ce
gsion1 <gsion@thingva.com>
2022-11-13 13:57:29 +0100

    Give focus to calculator automatically

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,15 @@
 function toggleCalculator(){
-    //let position = parseFloat(document.getElementById('calculator_table').style.right);
-    //var ifrm = document.getElementById('embed_calculator')
-    //var doc = ifrm.contentDocument? ifrm.contentDocument:
-        //ifrm.contentWindow.document
     let position = parseFloat(document.getElementById('embed_calculator').style.right);
-    console.log(position)
-    if(position >= 0){
-     document.getElementById('embed_calculator').style.right = "-305px";
-    } else {
-     document.getElementById('embed_calculator').style.right = "5px";
+
+    if(position >= 0) //close calculator pannel
+    {
+        document.getElementById('embed_calculator').style.right = "-305px";
+    } 
+    else //open calculator pannel
+    { 
+        document.getElementById('embed_calculator').style.right = "5px";
+        //giving focus to calclator display doens't work locally, please use a web server (for ex, do in terminal $ python3 -m http.server 8080)
+        document.getElementById("embed_calculator").contentWindow.document.getElementById('calculator_display').focus()  
     }
 }
 
